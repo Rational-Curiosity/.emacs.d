df9756d94a091cada0f952c1d49f1afdcb14c324
Curiosidad-Racional <hecenra@gmail.com>
2017-09-22 19:38:31 +0200

    recentf depends on machine config

--- config/menu-config.el
+++ config/menu-config.el
@@ -3,7 +3,9 @@
       recentf-max-menu-items 20
       recentf-exclude '("\\.emacs\\.d/elpa/.*\\.el\\'")
       recentf-filename-handlers '(file-truename abbreviate-file-name))
-(recentf-mode 1)
+;; There are machine dependent configurations
+(with-eval-after-load 'machine-config
+  (recentf-mode 1))
 ;; [ Se encarga helm
 ;;(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 ;; ]
