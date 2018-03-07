dc0cba6b4dd630e4a76e6d925c3e21d8ff9ef6f5
Curiosidad-Racional <hecenra@gmail.com>
2018-03-07 21:01:14 +0100

    remove obsolete helm-swoop

--- config/helm-extensions-config.el
+++ config/helm-extensions-config.el
@@ -96,82 +96,82 @@
 ;; ]
 
 ;;;;;;;;;;;
-;; Swoop ;;
+;; occur ;;
 ;;;;;;;;;;;
-(require 'helm-swoop)
+;; (require 'helm-swoop)
 
 ;; Change the keybinds to whatever you like :)
-(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-i") 'helm-occur)
 (global-set-key (kbd "M-I") (lambda ()
                               (interactive)
                               (mark-thing 'symbol)
-                              (helm-swoop)))
-(global-set-key (kbd "C-x M-i") 'helm-swoop-back-to-last-point)
-(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+                              (helm-occur)))
+;; (global-set-key (kbd "C-x M-i") 'helm-swoop-back-to-last-point)
+;; (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
 (global-set-key (kbd "C-v") nil)
-(global-set-key (kbd "C-v M-i") 'helm-multi-swoop-all)
+;; (global-set-key (kbd "C-v M-i") 'helm-multi-swoop-all)
 
-;; When doing isearch, hand the word over to helm-swoop
-(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-;; From helm-swoop to helm-multi-swoop-all
-(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-;; When doing evil-search, hand the word over to helm-swoop
-;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+;; ;; When doing isearch, hand the word over to helm-swoop
+;; (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+;; ;; From helm-swoop to helm-multi-swoop-all
+;; (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+;; ;; When doing evil-search, hand the word over to helm-swoop
+;; ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
 
-;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
-(define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+;; ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+;; (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
 
-;; Move up and down like isearch
-(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-(define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
-(define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
+;; ;; Move up and down like isearch
+;; (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+;; (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+;; (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+;; (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
 
-;; Save buffer when helm-multi-swoop-edit complete
-(setq helm-multi-swoop-edit-save t)
+;; ;; Save buffer when helm-multi-swoop-edit complete
+;; (setq helm-multi-swoop-edit-save t)
 
-;; If this value is t, split window inside the current window
-(setq helm-swoop-split-with-multiple-windows t)
+;; ;; If this value is t, split window inside the current window
+;; (setq helm-swoop-split-with-multiple-windows t)
 
-;; Split direcion. 'split-window-vertically or 'split-window-horizontally
-(setq helm-swoop-split-direction 'split-window-vertically)
+;; ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+;; (setq helm-swoop-split-direction 'split-window-vertically)
 
-;; If nil, you can slightly boost invoke speed in exchange for text color
-(setq helm-swoop-speed-or-color nil)
+;; ;; If nil, you can slightly boost invoke speed in exchange for text color
+;; (setq helm-swoop-speed-or-color nil)
 
-;; ;; Go to the opposite side of line from the end or beginning of line
-(setq helm-swoop-move-to-line-cycle t)
+;; ;; ;; Go to the opposite side of line from the end or beginning of line
+;; (setq helm-swoop-move-to-line-cycle t)
 
-;; Optional face for line numbers
-;; Face name is `helm-swoop-line-number-face`
-(setq helm-swoop-use-line-number-face nil)
+;; ;; Optional face for line numbers
+;; ;; Face name is `helm-swoop-line-number-face`
+;; (setq helm-swoop-use-line-number-face nil)
 
-;; If you prefer fuzzy matching
-(setq helm-swoop-use-fuzzy-match nil)
+;; ;; If you prefer fuzzy matching
+;; (setq helm-swoop-use-fuzzy-match nil)
 
-;; This function can pre-input keywords when helm-swoop invoked
-(setq helm-swoop-pre-input-function
-      (lambda ()
-        (if (use-region-p)
-            (buffer-substring-no-properties (region-beginning) (region-end)))))
+;; ;; This function can pre-input keywords when helm-swoop invoked
+;; (setq helm-swoop-pre-input-function
+;;       (lambda ()
+;;         (if (use-region-p)
+;;             (buffer-substring-no-properties (region-beginning) (region-end)))))
 
-(dolist (mode-map (list helm-swoop-map helm-multi-swoop-map))
-  (bind-keys :map mode-map
-             ("C-x n" . (lambda ()
-                          (interactive)
-                          (insert "\\ .*")))
-             ("C-x i" . (lambda ()
-                          (interactive)
-                          (insert "\\ .*[0-9]+")))
-             ("C-x f" . (lambda ()
-                          (interactive)
-                          (insert "[0-9]*\\.[0-9]+\\|[0-9]+\\.")))
-             ("C-x d" . (lambda ()
-                          (interactive)
-                          (insert "[0-9]\\{4\\}-[0-9][0-9]-[0-9][0-9]")))
-             ("C-x D" . (lambda ()
-                          (interactive)
-                          (insert "[0-9]\\{8\\}")))))
+;; (dolist (mode-map (list helm-swoop-map helm-multi-swoop-map))
+;;   (bind-keys :map mode-map
+;;              ("C-x n" . (lambda ()
+;;                           (interactive)
+;;                           (insert "\\ .*")))
+;;              ("C-x i" . (lambda ()
+;;                           (interactive)
+;;                           (insert "\\ .*[0-9]+")))
+;;              ("C-x f" . (lambda ()
+;;                           (interactive)
+;;                           (insert "[0-9]*\\.[0-9]+\\|[0-9]+\\.")))
+;;              ("C-x d" . (lambda ()
+;;                           (interactive)
+;;                           (insert "[0-9]\\{4\\}-[0-9][0-9]-[0-9][0-9]")))
+;;              ("C-x D" . (lambda ()
+;;                           (interactive)
+;;                           (insert "[0-9]\\{8\\}")))))
 
 ;;;;;;;;;;;;;;;
 ;;  company  ;;
