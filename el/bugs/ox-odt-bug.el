22569c0a27f21dbd79d573252d0188c6be3f8d83
Curiosidad-Racional <hecenra@gmail.com>
2018-04-12 21:36:08 +0200

    fixing bugs with new style

--- el/bugs/ox-odt-bug.el
+++ el/bugs/ox-odt-bug.el
@@ -1,10 +1,16 @@
-(defun org-odt-verbatim (verbatim contents info)
-  "Transcode a VERBATIM object from Org to ODT.
+(if (string-equal
+     "\300\301\302\303\304\211;\203\0\305\306#\266\202\202\0\307A@\"\266\202!#\207"
+     (aref (symbol-function 'org-odt-verbatim) 1))
+    (defun org-odt-verbatim (verbatim contents info)
+      "Transcode a VERBATIM object from Org to ODT.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
-  (format "<text:span text:style-name=\"%s\">%s</text:span>"
-	  "OrgVerbatim" (org-odt--encode-plain-text
-		     (org-element-property :value verbatim))))
+      (format "<text:span text:style-name=\"%s\">%s</text:span>"
+              "OrgVerbatim" (org-odt--encode-plain-text
+                             (org-element-property :value verbatim))))
+  (message "WARN ox-odt-bug.el: bug fixed for different version of org-odt-verbatim"))
+
+
 
 
 (provide 'ox-odt-bug)
