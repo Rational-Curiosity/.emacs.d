fb040393c2b1cd4bb0287d3899ca9183fa7e03f6
Curiosidad-Racional <hecenra@gmail.com>
2018-04-16 23:34:41 +0200

    binary to text files

--- el/bugs/ox-odt-bug.el
+++ el/bugs/ox-odt-bug.el
@@ -1,6 +1,6 @@
 (when (bug-check-function-bytecode
        'org-odt-verbatim
-       "\300\301\302\303\304\211;\203\0\305\306#\266\202\202\0\307A@\"\266\202!#\207")
+       "wMHCw8QGB4k7gxYAxcYDAyO2goIeAMcBQUADIraCISOH")
   (defun org-odt-verbatim (verbatim contents info)
     "Transcode a VERBATIM object from Org to ODT.
 CONTENTS is nil.  INFO is a plist used as a communication
