717049d6eb506498bd88f87ade627a6658e4300a
Curiosidad-Racional <hecenra@gmail.com>
2018-10-18 02:56:27 +0200

    replace ediff with vdiff

--- el/config/diff-config.el
+++ el/config/diff-config.el
@@ -8,6 +8,12 @@
 (add-hook 'ediff-after-quit-hook-internal 'winner-undo)
 
 (custom-set-faces
+ '(diff-refine-changed 
+   ((((class color) (min-colors 88) (background light))
+     :background "#888833")
+    (((class color) (min-colors 88) (background dark))
+     :background "#555511")
+    (t :inverse-video t)))
  '(ediff-odd-diff-A ((t (:background "dark slate gray"))))
  '(ediff-odd-diff-B ((t (:background "dark slate gray"))))
  '(ediff-odd-diff-C ((t (:background "dark slate gray"))))
@@ -30,6 +36,7 @@
 ?$\\|[<>] \\|---\\|.*Warning *:\\|.*No +newline\\|.*missing +newline\\|.*No +hay +ningún +carácter +de +nueva +línea +al +final +del +fichero\\|^
 ?$\\)")
 
+(require 'vdiff)
 (defun diff-revert-buffer-with-file (&optional arg)
   "Compare the current modified buffer with the saved version.
 ARG - `C-u' differ with prompted file.
@@ -44,11 +51,25 @@ ARG - `C-u' differ with prompted file.
    (t
     (vdiff-current-file))))
 
-(require 'vdiff)
+(defun vdiff-hydra-or-diff (&optional arg)
+  (interactive "P")
+  (condition-case nil
+      (call-interactively 'vdiff-hydra/body)
+    (error
+     (cond
+      ((equal arg '(64))
+       (call-interactively 'vdiff-files3))
+      ((equal arg '(16))
+       (call-interactively 'vdiff-buffers3))
+      ((equal arg '(4))
+       (call-interactively 'vdiff-files))
+      (t
+       (call-interactively 'vdiff-buffers)))
+     (call-interactively 'vdiff-hydra/body))))
 
 (bind-keys
  ("C-c d R"     . diff-revert-buffer-with-file)
- ("C-c d m"     . vdiff-hydra/body)
+ ("C-c d m"     . vdiff-hydra-or-diff)
  ("C-c d 3 f"   . vdiff-files3)
  ("C-c d f"     . vdiff-files)
  ("C-c d 3 b"   . vdiff-buffers3)
