91eb313d841a6c6db577b0a9b77d5926adb55504
Curiosidad-Racional <hecenra@gmail.com>
2019-05-15 18:12:50 +0200

    ensure disable ido occur preview

--- el/config/ido-occur.el
+++ el/config/ido-occur.el
@@ -64,33 +64,23 @@
 
 (defun ido-occur--decorate-lines-list (lines)
   "Add line number to each string in `LINES'."
-
   (let* ((lines-count (number-to-string (length lines))))
-
     (-map-indexed (lambda (index str)
                     "Transform `INDEX' to number and add to `STR'."
-
                     (let* ((line-number (+ index 1))
-
                            (number-length (number-to-string (length lines-count)))
-
                            (formated-line-number (format (concat "%" number-length "s")
                                                          line-number)))
-
                       (format "%s %s" formated-line-number str)))
-
                   lines)))
 
 (defun ido-occur--lines-as-list (buffer line-number)
   "List all lines of `BUFFER' with respects to current `LINE-NUMBER'.
 List lines from `LINE-NUMBER' to end of `BUFFER'
 and from end of `BUFFER' to beginning of `BUFFER'."
-
   (let ((line-number (line-number-at-pos))
-
         (lines (ido-occur--decorate-lines-list
                 (split-string (ido-occur--lines-as-string buffer) "\n"))))
-
     (-concat (-drop (- line-number 1) lines)
              (-take line-number lines))))
 
@@ -104,26 +94,24 @@ and from end of `BUFFER' to beginning of `BUFFER'."
 (defun ido-occur--run (&optional query)
   "Yet another `occur' with `ido'.
 When non-nil, QUERY is the initial search pattern."
-  (setq ido-occur--line-number (line-number-at-pos))
   (unwind-protect
-      (let* ((initial-column (current-column))
-
-             (line (ido-occur--strip-text-properties
-                    (ido-completing-read ido-occur--prompt
-                                         (ido-occur--lines-as-list (current-buffer)
-                                                                   ido-occur--line-number)
-                                         nil
-                                         nil
-                                         query)))
-             (line-length (length line))
-
-             (new-column (if (<= line-length initial-column)
-                             line-length
-                           initial-column))
-
-             (line-number (string-to-number (car (split-string line)))))
-        (forward-line (- line-number ido-occur--line-number))
-        (move-to-column new-column))
+      (progn
+        (setq ido-occur--line-number (line-number-at-pos))
+        (let* ((initial-column (current-column))
+               (line (ido-occur--strip-text-properties
+                      (ido-completing-read ido-occur--prompt
+                                           (ido-occur--lines-as-list (current-buffer)
+                                                                     ido-occur--line-number)
+                                           nil
+                                           nil
+                                           query)))
+               (line-length (length line))
+               (new-column (if (<= line-length initial-column)
+                               line-length
+                             initial-column))
+               (line-number (string-to-number (car (split-string line)))))
+          (forward-line (- line-number ido-occur--line-number))
+          (move-to-column new-column)))
     (setq ido-occur--line-number 0)))
 
 (defun ido-preview ()
@@ -160,36 +148,28 @@ When non-nil, QUERY is the initial search pattern."
 
   ;; Because in original "Ido" matcher preserves candidates sort order.
   (when (fboundp 'ido-clever-match-disable) (ido-clever-match-disable))
-
   (cond ((bound-and-true-p ido-vertical-mode)
          (ido-occur--run query))
-
         ((bound-and-true-p ido-grid-mode)
          (let ((ido-grid-mode-max-columns 1)
                (ido-grid-mode-max-rows 8)
                (ido-grid-mode-prefix-scrolls t))
            (ido-occur--run query)))
-
         (t
          (let ((ido-decorations ido-occur--decorations))
            (ido-occur--run query))))
-
   (when (fboundp 'ido-clever-match-enable) (ido-clever-match-enable)))
 
 ;;;###autoload
 (defun ido-occur-at-point ()
   "Open `ido-occur' at point."
-
   (interactive)
-
   (ido-occur (if (symbol-at-point) (symbol-name (symbol-at-point)) "")))
 
 ;;;###autoload
 (defun ido-occur-from-isearch ()
   "Open `ido-occur' from `isearch'."
-
   (interactive)
-
   (ido-occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))
 
 
