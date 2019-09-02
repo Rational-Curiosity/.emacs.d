843ed37c1316ba64bf8dfb45a5a43fd6487c59b7
Curiosidad-Racional <hecenra@gmail.com>
2019-09-03 00:18:27 +0200

    minor changes

--- el/config/vlf-config.el
+++ el/config/vlf-config.el
@@ -11,14 +11,15 @@
 
 (setq vlf-batch-size (* 1024 1025))
 
-(defun my-find-file-check-make-large-file-read-only-hook ()
+(defun find-file-check-make-large-file-read-only-hook ()
   "If a file is over a given size, make the buffer read only."
   (when (> (buffer-size) vlf-batch-size)
     (setq buffer-read-only t)
     (buffer-disable-undo)
     (fundamental-mode)
     (message "Big file: Read-only, fundamental mode & undo disabled.")))
+(add-hook 'find-file-hook 'find-file-check-make-large-file-read-only-hook)
 
-(add-hook 'find-file-hook 'my-find-file-check-make-large-file-read-only-hook)
+
 (provide 'vlf-config)
 ;;; vlf-config.el ends here
