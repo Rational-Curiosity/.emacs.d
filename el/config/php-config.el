00e6298e1c73daadb9acb75949650d97c3779aef
Curiosidad-Racional <hecenra@gmail.com>
2019-10-19 00:55:50 +0200

    php find definition support

--- el/config/php-config.el
+++ el/config/php-config.el
@@ -17,12 +17,28 @@
 ;;;; Install dependencies
 ;; sudo apt install php<x.x>
 
+;;;; Configure
+;; cd <project root>
+;; touch .ac-php-conf.json
+
 ;;; Code:
 
 (message "Importing php-config")
 
 (when (load "company-php" t)
-  (add-to-list 'company-backends 'company-ac-php-backend))
+  ;; Enable ElDoc support (optional)
+  (ac-php-core-eldoc-setup)
+  (add-to-list 'company-backends 'company-ac-php-backend)
+  ;; Jump to definition (optional)
+  (define-key php-mode-map (kbd "M-.") 'ac-php-find-symbol-at-point)
+
+  ;; Return back (optional)
+  (define-key php-mode-map (kbd "M-,") 'ac-php-location-stack-back))
+
+;;;;;;;;;;
+;; Keys ;;
+;;;;;;;;;;
+(define-key php-mode-map (kbd "M-b") nil)
 
 
 (provide 'php-config)
