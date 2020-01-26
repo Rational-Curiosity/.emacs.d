ea0714aa1dda0257f9a1d9e879a3271315f8ff4f
Curiosidad-Racional <hecenra@gmail.com>
2020-01-26 01:38:49 +0100

    lsp ignore tmp folders

--- el/config/lsp-config.el
+++ el/config/lsp-config.el
@@ -31,7 +31,10 @@
 
 (setq lsp-prefer-flymake nil
       lsp-ui-doc-position 'top
-      lsp-ui-doc-alignment 'frame)
+      lsp-ui-doc-alignment 'frame
+      lsp-file-watch-ignored
+      (cons "[/\\\\]tmp$"
+            lsp-file-watch-ignored))
 
 (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
 (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
