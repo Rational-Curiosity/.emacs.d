031703010a085efa4cd38c6e1b10279455f97c75
Curiosidad-Racional <hecenra@gmail.com>
2020-03-07 01:00:38 +0100

    hideshow config file

---
+++ el/config/hideshow-config.el
@@ -0,0 +1,34 @@
+;;; hideshow-config.el --- Configure and improve hideshow
+
+;;; Commentary:
+
+;; Usage:
+;; (with-eval-after-load 'hideshow
+;;   (require 'hideshow-config))
+;; never:
+;; (require 'hideshow-config)
+
+;; Do not include in this file:
+;; (require 'hideshow-mode)
+
+;;; Code:
+
+(message "Importing hideshow-config")
+
+(setcar (cdr (assq 'hs-minor-mode minor-mode-alist)) "")
+
+(with-eval-after-load 'tex-mode
+  (add-hook 'latex-mode-hook #'hs-minor-mode)
+  (define-key latex-mode-map (kbd "C-+") #'hs-toggle-hiding)
+  (add-hook 'tex-mode-hook #'hs-minor-mode)
+  (define-key tex-mode-map (kbd "C-+") #'hs-toggle-hiding))
+(with-eval-after-load 'latex
+  (add-hook 'LaTeX-mode-hook #'hs-minor-mode)
+  (define-key LaTeX-mode-map (kbd "C-+") #'hs-toggle-hiding))
+(with-eval-after-load 'tex
+  (add-hook 'TeX-mode-hook #'hs-minor-mode)
+  (define-key TeX-mode-map (kbd "C-+") #'hs-toggle-hiding))
+
+
+(provide 'hideshow-config)
+;;; hideshow-config.el ends here
