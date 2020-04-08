7a3a2e0e57e7c2c58d6b01512889363a7c3d59d2
Curiosidad-Racional <hecenra@gmail.com>
2020-04-08 17:13:17 +0200

    new tags and fast tags selection

--- el/config/elfeed-config.el
+++ el/config/elfeed-config.el
@@ -15,40 +15,105 @@
 
 (message "Importing elfeed-config")
 
+(defface elfeed-search-science-title-face
+  '((((class color) (background light)) (:foreground "#fd0"))  ;; gold
+    (((class color) (background dark))  (:foreground "#fd0")))
+  "Face used in search mode for titles."
+  :group 'elfeed)
+
+(defface elfeed-search-health-title-face
+  '((((class color) (background light)) (:foreground "#fcd"))  ;; pink
+    (((class color) (background dark))  (:foreground "#fcd")))
+  "Face used in search mode for titles."
+  :group 'elfeed)
+
 (defface elfeed-search-audio-title-face
-  '((((class color) (background light)) (:foreground "#f77"))
-    (((class color) (background dark))  (:foreground "#f77")))
+  '((t  (:background "#3d3")))  ;; lime green
   "Face used in search mode for titles."
   :group 'elfeed)
 
 (defface elfeed-search-image-title-face
-  '((((class color) (background light)) (:foreground "#077"))
-    (((class color) (background dark))  (:foreground "#077")))
+  '((t  (:background "#ad3")))  ;; yellow green
   "Face used in search mode for titles."
   :group 'elfeed)
 
-(push '(audio elfeed-search-audio-title-face) elfeed-search-face-alist)
-(push '(image elfeed-search-image-title-face) elfeed-search-face-alist)
+(defface elfeed-search-video-title-face
+  '((t  (:background "#a33")))  ;; brown
+  "Face used in search mode for titles."
+  :group 'elfeed)
+
+(push '(aud elfeed-search-audio-title-face) elfeed-search-face-alist)
+(push '(img elfeed-search-image-title-face) elfeed-search-face-alist)
+(push '(vid elfeed-search-video-title-face) elfeed-search-face-alist)
+(push '(science elfeed-search-science-title-face) elfeed-search-face-alist)
+(push '(health elfeed-search-health-title-face) elfeed-search-face-alist)
+
+(defun elfeed-youtube-expand (id)
+  (format
+   (pcase (substring id 0 2)
+     ("UC" "https://www.youtube.com/feeds/videos.xml?channel_id=%s")
+     ("PL" "https://www.youtube.com/feeds/videos.xml?playlist_id=%s")
+     (_    "https://www.youtube.com/feeds/videos.xml?user=%s"))
+   id))
 
 (setq elfeed-feeds
-      '(("https://e00-expansion.uecdn.es/rss/portada.xml" expansion spanish paper)
-        ("https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml" elmundo spanish paper)
-        ("https://www.abc.es/rss/feeds/abc_ultima.xml" abc spanish paper)
-        ("http://ep00.epimg.net/rss/tags/ultimas_noticias.xml" elpais spanish paper)
-        ("http://feeds.bbci.co.uk/news/rss.xml?edition=int" bbc english paper)
-        ("https://podcasts.files.bbci.co.uk/p02pc9ny.rss" bbc english audio)
-        ("https://www.theguardian.com/international/rss" theguardian english paper)
-        ("https://www.theguardian.com/news/series/todayinfocus/podcast.xml" theguardian english audio)
-        ("https://rss.nytimes.com/services/xml/rss/nyt/World.xml" nytimes world english paper)
-        ("https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml" nytimes europe english paper)
-        ("https://www.sciencenews.org/feed" sciencenews english paper)
-        ("https://xkcd.com/atom.xml" xkcd english image))
+      `(("https://e00-expansion.uecdn.es/rss/portada.xml" expansion es txt)
+        ("http://estaticos.elmundo.es/elmundo/rss/espana.xml" elmundo spain es txt)
+        ("http://estaticos.elmundo.es/elmundo/rss/internacional.xml" elmundo world es txt)
+        ("http://www.abc.es/rss/feeds/abc_EspanaEspana.xml" abc spain es txt)
+        ("http://www.abc.es/rss/feeds/abc_Internacional.xml" abc world es txt)
+        ("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada" elpais spain es txt)
+        ("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/internacional/portada" elpais world es txt)
+        ;; english
+        ("http://feeds.bbci.co.uk/news/rss.xml?edition=int" bbc world en txt)
+        ("https://www.theguardian.com/international/rss" theguardian world en txt)
+        ("https://rss.nytimes.com/services/xml/rss/nyt/World.xml" nytimes world en txt)
+        ;; science
+        ("http://rss.sciam.com/ScientificAmerican-Global" SA science en txt)
+        ("https://www.sciencenews.org/feed" sciencenews science en txt)
+        ;; health
+        ("https://www.who.int/rss-feeds/news-english.xml" oms health es txt)
+        ;; podcast
+        ("https://podcasts.files.bbci.co.uk/p02pc9ny.rss" bbc en aud)
+        ("https://www.theguardian.com/news/series/todayinfocus/podcast.xml" theguardian en aud)
+        ;; images
+        ("https://xkcd.com/atom.xml" xkcd en img)
+        ;; video
+        (,(elfeed-youtube-expand "UCHnyfMqiRRG1u-2MsSQLbXA") veritasium science en vid)
+        (,(elfeed-youtube-expand "UCW3iqZr2cQFYKdO9Kpa97Yw") utbh es vid))
       elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory)
-      elfeed-search-filter "@1-month-ago +unread")
+      elfeed-search-filter "@1-month-ago +unread"
+      elfeed-search-date-format '("%Y-%m-%d %H:%M" 16 :left)
+      elfeed-search-trailing-width 30
+      elfeed-search-title-min-width 20
+      elfeed-search-title-max-width 100)
 
+(setq elfeed-search--tags
+      (let ((tags #s(hash-table size 30 test eq data (unread t))))
+        (dolist (item elfeed-feeds)
+          (if (listp item)
+              (mapc (lambda (tag) (puthash tag t tags))
+                    (cdr item))))
+        (mapcar 'symbol-name
+                (hash-table-keys tags))))
 
+(defun elfeed-search-filter-tags-selection (arg)
+  (interactive "P")
+  (let ((pre (if arg "-" "+"))
+        (tag (completing-read "Select tag: " elfeed-search--tags nil t)))
+    (elfeed-search-set-filter
+     (if (string-match (concat " ?[-+]" tag) elfeed-search-filter)
+         (replace-match "" t nil elfeed-search-filter)
+       (concat elfeed-search-filter " " pre tag)))))
+
+(define-key elfeed-show-mode-map "h" nil)
+(define-key elfeed-show-mode-map "?" #'describe-mode)
 (define-key elfeed-show-mode-map "R" #'language-text-to-speak-region)
 (define-key elfeed-show-mode-map "S" #'language-text-to-speak-stop)
+
+(define-key elfeed-search-mode-map "h" nil)
+(define-key elfeed-search-mode-map "?" #'describe-mode)
+(define-key elfeed-search-mode-map "t" 'elfeed-search-filter-tags-selection)
 
 
 (provide 'elfeed-config)
