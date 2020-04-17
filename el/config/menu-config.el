97a992e28acd8715b2acd0db57a1f0139dc4fcb7
Curiosidad-Racional <hecenra@gmail.com>
2020-04-17 13:07:25 +0200

    fixed recentf tramp integration problems

--- el/config/menu-config.el
+++ el/config/menu-config.el
@@ -9,29 +9,44 @@
 
 (require 'recentf)
 
-(defun recentf-remove-sudo-tramp-prefix-remove-sudo (x)
-  "Remove sudo from path.  Argument X is path."
-  (if (tramp-tramp-file-p x)
-      (let ((tx (tramp-dissect-file-name x)))
-        (if (string-equal "sudo" (tramp-file-name-method tx))
-            (tramp-file-name-localname tx) x))
-    x))
+(add-hook 'after-init-hook 'recentf-cleanup)
 
-(setq recentf-max-saved-items 500
-      recentf-max-menu-items 30
-      recentf-exclude '("\\.emacs\\.d/elpa/.*\\.el\\'" "\\.el\\.gz\\'")
-      recentf-filename-handlers '(recentf-remove-sudo-tramp-prefix-remove-sudo
-                                  file-truename
-                                  abbreviate-file-name)
-      ;; recentf-keep '()
-      tool-bar-max-label-size 12
-      tool-bar-style 'image)
-;; There are machine dependent configurations
 ;; (with-eval-after-load 'machine-config
 ;;   (cl-letf (((symbol-function 'sit-for)
 ;;              (lambda (secs))))
 ;;     (let ((tramp-message-show-message nil))
 ;;       (recentf-mode 1))))
+
+
+(defun recentf-remove-sudo-tramp-prefix (path)
+  "Remove sudo from path.  Argument PATH is path."
+  (if (tramp-tramp-file-p path)
+      (let ((tx (tramp-dissect-file-name path)))
+        (pcase (tramp-file-name-method tx)
+          ("sudo" (tramp-file-name-localname tx))
+          ("docker" (if (featurep 'docker) path (tramp-file-name-localname)))
+          (_ path)))
+    path))
+
+(defun local-file-exists-p (filename)
+  (file-exists-p (recentf-remove-sudo-tramp-prefix filename)))
+
+(defun recentf-file-truename (filename)
+  (let* ((local-file-name (recentf-remove-sudo-tramp-prefix filename))
+         (local-file-truename (file-truename local-file-name)))
+    (concat (substring filename 0 (- (length local-file-name))) local-file-truename)))
+
+(setq recentf-max-saved-items 500
+      recentf-max-menu-items 30
+      recentf-exclude '("\\.emacs\\.d/elpa/.*\\.el\\'" "\\.el\\.gz\\'")
+      recentf-filename-handlers '(recentf-file-truename
+                                  abbreviate-file-name)
+      recentf-keep '(local-file-exists-p)
+      tool-bar-max-label-size 12
+      recentf-auto-cleanup 'never
+      tool-bar-style 'image)
+
+(recentf-mode 1)
 
 
 (provide 'menu-config)
