962b054db9e77715e55cad23bee81d2e8489ac04
Curiosidad-Racional <hecenra@gmail.com>
2020-04-18 14:01:58 +0200

    dap configurations

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
  '(custom-safe-themes
    '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(package-selected-packages
-   '(ace-window ag async auctex avy bash-completion bookmark+ cmake-font-lock cmake-mode company cyphejor dash dash-functional deferred docker docker-tramp edit-server elfeed emms epl expand-region exwm f figlet flycheck flyspell-correct flyspell-correct-helm free-keys git-commit gnuplot gnuplot-mode go-mode goto-chg graphviz-dot-mode guess-language haskell-mode helm helm-ag helm-company helm-core helm-etags-plus helm-exwm helm-flycheck helm-flyspell helm-lsp helm-org helm-projectile helm-swoop helm-tramp helm-unicode helm-xref hide-comnt highlight hl-line+ hl-todo ht htmlize hydra imenu-anywhere json-mode json-reformat json-snatcher let-alist link-hint lsp-mode lsp-ui lua-mode lv magit magit-todos map markdown-mode markdown-mode+ memoize mini-modeline multiple-cursors ob-async org org-agenda-property org-brain org-plus-contrib org-ql org-super-agenda org-superstar ov ox-gfm ox-mediawiki ox-rst ox-twbs pcre2el peg php-mode pkg-info plantuml-mode popup projectile protobuf-mode psession rainbow-delimiters rebox2 request request-deferred rust-mode s smartparens smartscan spinner stickyfunc-enhance string-inflection swap-regions tablist thingatpt+ transient transpose-frame ts undo-tree vdiff vimish-fold virtualenvwrapper vlf web-mode which-key with-editor xahk-mode xelb xterm-color yasnippet yasnippet-snippets))
+   '(ace-window ag async auctex avy bash-completion bookmark+ bui cmake-font-lock cmake-mode company cyphejor dap-mode dash dash-functional deferred docker docker-tramp edit-server elfeed emms epl expand-region exwm f figlet flycheck flyspell-correct flyspell-correct-helm free-keys git-commit gnuplot gnuplot-mode go-mode goto-chg graphviz-dot-mode guess-language haskell-mode helm helm-ag helm-company helm-core helm-etags-plus helm-exwm helm-flycheck helm-flyspell helm-lsp helm-org helm-projectile helm-swoop helm-tramp helm-unicode helm-xref hide-comnt highlight hl-line+ hl-todo ht htmlize hydra imenu-anywhere json-mode json-reformat json-snatcher let-alist link-hint lsp-java lsp-mode lsp-treemacs lsp-ui lua-mode lv magit magit-todos map markdown-mode markdown-mode+ memoize mini-modeline multiple-cursors ob-async org org-agenda-property org-brain org-plus-contrib org-ql org-super-agenda org-superstar ov ox-gfm ox-mediawiki ox-rst ox-twbs pcre2el peg pfuture php-mode pkg-info plantuml-mode popup projectile protobuf-mode psession rainbow-delimiters rebox2 request request-deferred rust-mode s smartparens smartscan spinner stickyfunc-enhance string-inflection swap-regions tablist thingatpt+ transient transpose-frame treemacs ts undo-tree vdiff vimish-fold virtualenvwrapper vlf web-mode which-key with-editor xahk-mode xelb xterm-color yasnippet yasnippet-snippets))
  '(safe-local-variable-values
    '((eval set 'org-agenda-files
            (list
@@ -308,22 +308,41 @@
 (add-hook 'python-mode-hook #'lsp-deferred)
 (setq python-shell-interpreter "python3")
 (with-eval-after-load 'python
-;;  (require 'semantic/wisent/python)
+  ;;  (require 'semantic/wisent/python)
+  (when (load "dap-python" t)
+    (dap-ui-mode)
+    (dap-tooltip-mode))
   (require 'python-config)
-;;  (add-hook 'python-mode-hook #'detect-python-project-version)
+  ;;  (add-hook 'python-mode-hook #'detect-python-project-version)
   )
 (with-eval-after-load 'virtualenvwrapper
   (require 'virtualenvwrapper-config))
 ;; ]
 
+;; [
+(add-hook 'java-mode-hook (lambda ()
+                            (when (load "lsp-java" t)
+                              (when (load "dap-java" t)
+                                (dap-ui-mode)
+                                (dap-tooltip-mode))
+                              (lsp-deferred))))
+;; ]
 
 ;; [ javascript
 (add-hook 'js-mode-hook #'lsp-deferred)
+(with-eval-after-load 'js
+  (when (load "dap-firefox" t)
+    (dap-firefox-setup)
+    (dap-ui-mode)
+    (dap-tooltip-mode)))
 ;; ]
 
 ;; [ php
 (add-hook 'php-mode-hook #'lsp-deferred)
 (with-eval-after-load 'php-mode
+  (when (load "dap-php" t)
+    (dap-ui-mode)
+    (dap-tooltip-mode))
   (require 'php-config))
 ;; ]
 
