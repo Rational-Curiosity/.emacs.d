59780bfd99431a4c0c9bbf9731e54dce193e1360
Curiosidad-Racional <hecenra@gmail.com>
2020-05-01 23:17:05 +0200

    org-brain with helm-org-rifle and org-noter

--- el/config/org-brain-config.el
+++ el/config/org-brain-config.el
@@ -31,5 +31,34 @@
 (push 'org-brain-entry-tags-string org-brain-vis-current-title-append-functions)
 
 
+(when (require 'helm-org-rifle nil t)
+  (defun helm-org-rifle-brain ()
+    "Rifle files in `org-brain-path'."
+    (interactive)
+    (let ((helm-org-rifle-close-unopened-file-buffers nil))
+      (helm-org-rifle-directories (list org-brain-path))))
+
+  (defun helm-org-rifle-open-in-brain (candidate)
+    (-let (((buffer . pos) candidate))
+      (with-current-buffer buffer
+        (goto-char pos)
+        (org-brain-visualize-entry-at-pt))))
+
+  (add-to-list 'helm-org-rifle-actions
+               (cons "Show entry in org-brain" 'helm-org-rifle-open-in-brain)
+               t))
+
+(when (require 'org-noter nil t)
+  (add-hook 'org-noter-insert-heading-hook #'org-id-get-create)
+  (defun org-brain-open-org-noter (entry)
+    "Open `org-noter' on the ENTRY.
+If run interactively, get ENTRY from context."
+    (interactive (list (org-brain-entry-at-pt)))
+    (org-with-point-at (org-brain-entry-marker entry)
+      (org-noter)))
+
+  (define-key org-brain-visualize-mode-map (kbd "\C-c n") 'org-brain-open-org-noter))
+
+
 (provide 'org-brain-config)
 ;;; org-brain-config.el ends here
