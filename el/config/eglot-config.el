c3665fd729d4151bcbf615daaa9a6df72ab5951e
Curiosidad-Racional <hecenra@gmail.com>
2020-10-16 19:30:19 +0200

    eglot first configuration

---
+++ el/config/eglot-config.el
@@ -0,0 +1,27 @@
+;;; eglot-config.el --- Configure and improve eglot
+
+;;; Commentary:
+
+;; Usage:
+;; (with-eval-after-load 'eglot-mode
+;;   (require 'eglot-config))
+;; never:
+;; (require 'eglot-config)
+
+;; Do not include in this file:
+;; (require 'eglot-mode)
+
+;;; Code:
+
+(message "Importing eglot-config")
+
+(setq mode-line-misc-info
+      (delq (assoc 'eglot--managed-mode
+                   mode-line-misc-info)
+            mode-line-misc-info)
+      eglot-sync-connect nil)
+
+
+
+(provide 'eglot-config)
+;;; eglot-config.el ends here
