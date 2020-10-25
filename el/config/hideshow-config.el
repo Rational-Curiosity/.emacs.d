541f55f16fd4cb8aa49daade316b12d6b99bcf72
Curiosidad-Racional <hecenra@gmail.com>
2020-10-25 18:18:20 +0100

    changed hideshow key bindings

--- el/config/hideshow-config.el
+++ el/config/hideshow-config.el
@@ -19,16 +19,13 @@
 
 (with-eval-after-load 'tex-mode
   (add-hook 'latex-mode-hook #'hs-minor-mode)
-  (define-key latex-mode-map (kbd "C-+") #'hs-toggle-hiding)
-  (add-hook 'tex-mode-hook #'hs-minor-mode)
-  (define-key tex-mode-map (kbd "C-+") #'hs-toggle-hiding))
+  (add-hook 'tex-mode-hook #'hs-minor-mode))
 (with-eval-after-load 'latex
-  (add-hook 'LaTeX-mode-hook #'hs-minor-mode)
-  (define-key LaTeX-mode-map (kbd "C-+") #'hs-toggle-hiding))
+  (add-hook 'LaTeX-mode-hook #'hs-minor-mode))
 (with-eval-after-load 'tex
-  (add-hook 'TeX-mode-hook #'hs-minor-mode)
-  (define-key TeX-mode-map (kbd "C-+") #'hs-toggle-hiding))
+  (add-hook 'TeX-mode-hook #'hs-minor-mode))
 
+(define-key hs-minor-mode-map (kbd "<C-tab>") #'hs-toggle-hiding)
 
 (provide 'hideshow-config)
 ;;; hideshow-config.el ends here
