c3bcff3e1e33a807396f3be4f76717fdc53e1f3f
Curiosidad-Racional <hecenra@gmail.com>
2020-10-26 17:57:24 +0100

    objed configurations for which-key & visual-line-mode

--- el/config/objed-config.el
+++ el/config/objed-config.el
@@ -11,6 +11,10 @@
 ;; sudo apt install ripgrep
 ;;; Code:
 
+(require 'objed)
+
+;; (add-hook 'prog-mode-hook 'objed-local-mode)
+
 (setq objed-disabled-modes
       '(exwm-mode
         browse-kill-ring-mode
@@ -24,6 +28,19 @@
       objed-cursor-color "#ff8c00"
       objed-use-hl nil)
 
+(push '(end-of-visual-line . line) objed-cmd-alist)
+(push '(beginning-of-visual-line . line) objed-cmd-alist)
+
+(with-eval-after-load 'which-key
+  (which-key-add-key-based-replacements "=" "avy inside")
+  (which-key-add-key-based-replacements "c" "choose type")
+  (which-key-add-key-based-replacements "x" "additional op")
+  (which-key-add-key-based-replacements "#" "avy other")
+  (which-key-add-key-based-replacements "`" "forward boundary")
+  (which-key-add-key-based-replacements "´" "backward boundary")
+  (which-key-add-key-based-replacements "'" "user map")
+  (which-key-add-key-based-replacements "-" "other user map"))
+(global-set-key (kbd "M-SPC") 'objed-activate)
 (objed-mode)
 
 
