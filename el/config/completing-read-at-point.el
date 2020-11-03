0b49852a5c11477d7a3139570d58a97c91bd93d1
Curiosidad-Racional <hecenra@gmail.com>
2020-11-03 18:23:00 +0100

    fix completion candidate insertion

---
+++ el/config/completing-read-at-point.el
@@ -0,0 +1,102 @@
+;;; completing-read-at-point.el --- completion-at-point with completing-read
+
+;;; Commentary:
+
+;; thanks to: https://github.com/katspaugh/ido-at-point
+
+;; Usage:
+
+;;
+;; (require 'completing-read-at-point)
+
+;;; Code:
+
+(defun completing-read-at-point-complete (start end collection &optional predicate)
+  "Completion for symbol at point using `completing-read'."
+  (let* ((string (buffer-substring-no-properties start end))
+         (comps (completion-all-completions
+                 string
+                 collection predicate (- end start))))
+    ;; No candidates
+    (if (null comps)
+        (message "No matches")
+      (let* ((base-size (cdr (last comps)))
+             (common (substring-no-properties string base-size)))
+        ;; Remove the last non-nil element of a possibly improper list
+        (nconc comps nil)
+        (if (null (cdr comps))
+            ;; Single candidate
+            (completing-read-at-point-insert start end base-size (car comps))
+          ;; Many candidates
+          (completing-read-at-point-do-complete start end base-size comps common))))))
+
+(defun completing-read-at-point-do-complete (start end base-size comps common)
+  (run-with-idle-timer
+   0 nil
+   `(lambda ()
+      (let ((choice (completing-read "" (quote ,comps) nil t ,common)))
+        (when (stringp choice)
+          (completing-read-at-point-insert ,start ,end ,base-size choice))))))
+
+(defun completing-read-at-point-insert (start end base-size completion)
+  "Replaces text in buffer from END back to COMMON-LEN
+with COMPLETION."
+  (goto-char end)
+  (delete-region (+ start base-size)
+                 end)
+  (insert (substring-no-properties completion)))
+
+(defun completing-read-at-point-completion-in-region (&rest args)
+  (if (boundp 'completion-in-region-function)
+      (if (window-minibuffer-p)
+          (with-no-warnings
+            (apply #'completion--in-region args))
+        (apply #'completing-read-at-point-complete args))
+    (if (window-minibuffer-p)
+        (apply (car args) (cdr args))
+      (apply #'completing-read-at-point-complete (cdr args)))))
+
+(defvar completing-read-at-point-previous-completion-in-region-function nil)
+
+(defun completing-read-at-point-mode-set (enable)
+  (if (boundp 'completion-in-region-function)
+      (if enable
+          (setq completing-read-at-point-previous-completion-in-region-function
+                completion-in-region-function
+                completion-in-region-function
+                'completing-read-at-point-completion-in-region)
+        (setq completion-in-region-function
+              completing-read-at-point-previous-completion-in-region-function))
+    (with-no-warnings
+      (if enable
+          (add-to-list 'completion-in-region-functions
+                       'completing-read-at-point-completion-in-region)
+        (setq completion-in-region-functions
+              (delq 'completing-read-at-point-completion-in-region
+                    completion-in-region-functions))))))
+
+;;;###autoload
+(define-minor-mode completing-read-at-point-mode
+  "Global minor mode to use ido for `completion-at-point'.
+
+When called interactively, toggle `completing-read-at-point-mode'.  With
+prefix ARG, enable `completing-read-at-point-mode' if ARG is positive,
+otherwise disable it.
+
+When called from Lisp, enable `completing-read-at-point-mode' if ARG is
+omitted, nil or positive.  If ARG is `toggle', toggle
+`completing-read-at-point-mode'.  Otherwise behave as if called
+interactively.
+
+With `completing-read-at-point-mode' use ido for `completion-at-point'."
+  :variable ((if (boundp 'completion-in-region-function)
+                 (eq completion-in-region-function
+                     'completing-read-at-point-completion-in-region)
+               (with-no-warnings
+                 (memq 'completing-read-at-point-completion-in-region
+                       completion-in-region-functions)))
+             .
+             completing-read-at-point-mode-set))
+
+(provide 'completing-read-at-point)
+;;; completing-read-at-point.el ends here
--- el/config/fido-config.el
+++ el/config/fido-config.el
@@ -13,11 +13,25 @@
 
 (require 'icomplete)
 (require 'icomplete-vertical)
-(require 'fido-at-point)
+(require 'completing-read-at-point)
+(require 'orderless)
 
-(set-face-attribute 'icomplete-first-match nil :foreground "YellowGreen")
+(set-face-attribute 'icomplete-first-match nil :foreground "#cafd32")
 
-(setq icomplete-prospects-height 4)
+(setq icomplete-prospects-height 4
+      icomplete-separator " · "
+      ;; fido options
+      icomplete-tidy-shadowed-file-names t
+      icomplete-show-matches-on-no-input t
+      icomplete-hide-common-prefix nil
+      completion-styles '(orderless)
+      completion-flex-nospace nil
+      completion-category-defaults nil
+      completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      read-file-name-completion-ignore-case t
+      ;; orderless
+      orderless-matching-styles '(orderless-regexp))
 
 (cond ((executable-find "fdfind")
        (setq fd-dired-program "fdfind"
@@ -30,25 +44,37 @@
 
 (rg-enable-default-bindings (kbd "M-g A"))
 
-(defun insert-kill-ring-item (&optional arg)
+(defun icomplete-vertical-kill-ring-insert (&optional arg)
   "Insert item from kill-ring, selected with completion."
   (interactive "*p")
   (if (or (eq last-command 'yank)
           (if (active-minibuffer-window)
               (setq last-command 'yank)))
       (yank-pop arg)
-    (message "last: %s" last-command)
     (icomplete-vertical-do
         (:separator 'dotted-line :height 20)
-      (insert (completing-read "Yank: " kill-ring nil t)))))
+      (let ((candidate (completing-read "Yank: " kill-ring nil t)))
+        (when (and candidate (region-active-p))
+          ;; the currently highlighted section is to be replaced by the yank
+          (delete-region (region-beginning) (region-end)))
+        (insert candidate)))))
 
-(global-set-key (kbd "M-y") 'insert-kill-ring-item)
+(define-key icomplete-minibuffer-map (kbd "C-k") 'icomplete-fido-kill)
+(define-key icomplete-minibuffer-map (kbd "C-d") 'icomplete-fido-delete-char)
+(define-key icomplete-minibuffer-map (kbd "RET") 'icomplete-fido-ret)
+(define-key icomplete-minibuffer-map (kbd "C-m") 'icomplete-fido-ret)
+(define-key icomplete-minibuffer-map (kbd "DEL") 'icomplete-fido-backward-updir)
+(define-key icomplete-minibuffer-map (kbd "M-j") 'icomplete-fido-exit)
+(define-key icomplete-minibuffer-map (kbd "C-s") 'icomplete-forward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-r") 'icomplete-backward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-|") 'icomplete-vertical-toggle)
+(global-set-key (kbd "M-y") 'icomplete-vertical-kill-ring-insert)
 (global-set-key (kbd "M-g f") 'fd-dired)
 (global-set-key (kbd "M-g a") 'ripgrep-regexp)
 (global-set-key (kbd "M-s O") 'multi-occur)
 
-(fido-mode)
-(fido-at-point-mode)
+(icomplete-mode)
+(completing-read-at-point-mode)
 
 
 (provide 'fido-config)
