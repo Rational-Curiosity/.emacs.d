43fc021c8eca6b4b8c73393212ebfd34a2aeb8e7
Curiosidad-Racional <hecenra@gmail.com>
2020-12-06 15:50:41 +0100

    from completion-at-point annotations to completing-read annotations

--- el/config/completing-read-at-point.el
+++ el/config/completing-read-at-point.el
@@ -2,8 +2,6 @@
 
 ;;; Commentary:
 
-;; thanks to: https://github.com/katspaugh/ido-at-point
-
 ;; Usage:
 
 ;;
@@ -11,88 +9,78 @@
 
 ;;; Code:
 
-(defvar completing-read-at-point-backend nil)
+(defvar crap-backend nil)
 (defun completion--capf-wrapper-advice (orig-fun fun which)
   (let* ((res (funcall orig-fun fun which))
          (fn (car res)))
-    (if fn (setq completing-read-at-point-backend fn))
+    (if fn (setq crap-backend fn))
     res))
 
-(defun completing-read-at-point-complete (start end collection &optional predicate)
-  "Completion for symbol at point using `completing-read'."
-  (let* ((string (buffer-substring-no-properties start end))
-         (comps (completion-all-completions
-                 string
-                 collection predicate (- end start))))
-    ;; No candidates
-    (if (null comps)
-        (message "No matches")
-      (let* ((base-size (cdr (last comps)))
-             (common (substring-no-properties string base-size)))
-        ;; Remove the last non-nil element of a possibly improper list
-        (nconc comps nil)
-        (if (null (cdr comps))
-            ;; Single candidate
-            (completing-read-at-point-insert start end base-size (car comps))
-          ;; Many candidates
-          (completing-read-at-point-do-complete start end base-size comps common))))))
+(defun crap-completion-in-region
+    (start end collection &optional predicate)
+  (completion-in-region-mode -1)
+  (if (window-minibuffer-p)
+      (with-no-warnings
+        (funcall crap-previous-completion-in-region-function
+                 start end collection predicate))
+    (setq completion-extra-properties
+          (plist-put completion-extra-properties :exit-function
+                     `(lambda (proxy status)))
+          completion-in-region-mode-predicate
+          (lambda ()
+            t))
+    (let* ((string (buffer-substring-no-properties start end))
+           (comps (completion-all-completions
+                   string
+                   collection predicate (- end start)))
+           (base-size (cdr (last comps)))
+           (common (substring-no-properties string base-size))
+           (proxy (completing-read
+                   (if crap-backend
+                       (format
+                        "`%s' "
+                        crap-backend)
+                     "")
+                   `(lambda (string pred action)
+                      (with-current-buffer
+                          (if (minibufferp)
+                              (window-buffer (minibuffer-selected-window))
+                            (current-buffer))
+                        (if (eq action 'metadata)
+                            '(metadata
+                              (annotation-function
+                               .
+                               (lambda (proxy)
+                                 (concat
+                                  ;; (make-string (max 2 (- 20 (length proxy))) ? )
+                                  " -"
+                                  (funcall ,(plist-get completion-extra-properties
+                                                      :annotation-function)
+                                          proxy)))))
+                        (funcall ,collection
+                               string pred action))))
+                   predicate t
+                   common)))
+      (with-current-buffer (marker-buffer start)
+        (goto-char end)
+        (delete-region (+ start base-size) end)
+        (insert (substring-no-properties proxy))))))
 
-(defun completing-read-at-point-do-complete (start end base-size comps common)
-  (run-with-idle-timer
-   0 nil
-   `(lambda ()
-      (let ((choice (completing-read ,(if completing-read-at-point-backend
-                                          (format "`%s' " completing-read-at-point-backend)
-                                        "") (quote ,comps) nil t ,common)))
-        (when (stringp choice)
-          (completing-read-at-point-insert ,start ,end ,base-size choice))))))
-
-(defun completing-read-at-point-insert (start end base-size completion)
-  "Replaces text in buffer from END back to COMMON-LEN
-with COMPLETION."
-  (goto-char end)
-  (delete-region (+ start base-size)
-                 end)
-  (insert (substring-no-properties completion)))
-
-(defun completing-read-at-point-completion-in-region (&rest args)
-  (if (boundp 'completion-in-region-function)
-      (if (window-minibuffer-p)
-          (with-no-warnings
-            (apply #'completion--in-region args))
-        (apply #'completing-read-at-point-complete args))
-    (if (window-minibuffer-p)
-        (apply (car args) (cdr args))
-      (apply #'completing-read-at-point-complete (cdr args)))))
-
-(defvar completing-read-at-point-previous-completion-in-region-function nil)
+(defvar crap-previous-completion-in-region-function nil)
 
 (defun completing-read-at-point-mode-set (enable)
-  (if (boundp 'completion-in-region-function)
-      (if enable
+  (if enable
           (prog1
-              (setq completing-read-at-point-previous-completion-in-region-function
+              (setq crap-previous-completion-in-region-function
                     completion-in-region-function
                     completion-in-region-function
-                    'completing-read-at-point-completion-in-region)
+                    'crap-completion-in-region)
             (advice-add 'completion--capf-wrapper :around
                         'completion--capf-wrapper-advice))
         (advice-remove 'completion--capf-wrapper
                        'completion--capf-wrapper-advice)
         (setq completion-in-region-function
-              completing-read-at-point-previous-completion-in-region-function))
-    (with-no-warnings
-      (if enable
-          (prog1
-              (add-to-list 'completion-in-region-functions
-                           'completing-read-at-point-completion-in-region)
-            (advice-add 'completion--capf-wrapper :around
-                        'completion--capf-wrapper-advice))
-        (advice-remove 'completion--capf-wrapper
-                       'completion--capf-wrapper-advice)
-        (setq completion-in-region-functions
-              (delq 'completing-read-at-point-completion-in-region
-                    completion-in-region-functions))))))
+              crap-previous-completion-in-region-function)))
 
 ;;;###autoload
 (define-minor-mode completing-read-at-point-mode
@@ -108,12 +96,8 @@ omitted, nil or positive.  If ARG is `toggle', toggle
 interactively.
 
 With `completing-read-at-point-mode' use ido for `completion-at-point'."
-  :variable ((if (boundp 'completion-in-region-function)
-                 (eq completion-in-region-function
-                     'completing-read-at-point-completion-in-region)
-               (with-no-warnings
-                 (memq 'completing-read-at-point-completion-in-region
-                       completion-in-region-functions)))
+  :variable ((eq completion-in-region-function
+                 'completing-read-at-point-completion-in-region)
              . completing-read-at-point-mode-set))
 
 
